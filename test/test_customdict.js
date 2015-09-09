bced64cbdfdb10ceeab07be638a53a135b815f67
Piti Ongmongkolkul <piti118@gmail.com>
2015-09-10 00:52:12 +0700

    allow for additional words as array

--- test/test_customdict.js
+++ test/test_customdict.js
@@ -4,13 +4,22 @@ var expect = require("chai").expect
 
 describe("Wordcut with custom dictionary", function() {
 
-  beforeEach(function() {
-    wordcut.init(__dirname+'/customdict.txt', true);
+  it("should recognize custom dict as glob", function(){
+    wordcut.init(__dirname+'/custom*.txt', true);
+    var segmentedResult = wordcut.cutIntoArray("ฉันชอบกินข้าวอร่อยมากมาก");
+    expect(segmentedResult).to.deep.equal(["ฉัน","ชอบ","กินข้าว","อร่อยมากมาก"])
   });
 
   it("should recognize word in custom dict", function(){
+    wordcut.init(__dirname+'/customdict.txt', true);
     var segmentedResult = wordcut.cutIntoArray("ฉันชอบกินข้าวอร่อยมากมาก");
     expect(segmentedResult).to.deep.equal(["ฉัน","ชอบ","กินข้าว","อร่อยมากมาก"])
+  });
+
+  it("should recognize word in custom dict and additionalWords", function(){
+    wordcut.init(__dirname+'/customdict.txt', true, ["ข้าวอร่อยมากมาก","ชอบกิน"]);
+    var segmentedResult = wordcut.cutIntoArray("ฉันชอบกินข้าวอร่อยมากมาก");
+    expect(segmentedResult).to.deep.equal(["ฉัน","ชอบกิน", "ข้าวอร่อยมากมาก"])
   });
 
 
